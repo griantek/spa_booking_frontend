0efda1319dc6581b0242c90f3b37f72a4ca35bd7
Muhammed Irshad Ali K K <75845752+M0I0A@users.noreply.github.com>
2024-12-11 14:59:45 +0530

    added

--- src/components/Register.js
+++ src/components/Register.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from "react";
 import axios from "axios";
 import { useNavigate, useLocation } from "react-router-dom";
 import "../styles/global.css";
+import { useSearchParams } from "react-router-dom";
 
 const Register = () => {
   const location = useLocation();
