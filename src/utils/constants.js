1569960fd135efae0947f5658258dd0a8b2c936c
Muhammed Irshad Ali K K <75845752+M0I0A@users.noreply.github.com>
2024-12-13 16:43:58 +0530

    url changed

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,5 +1,5 @@
 export const API_URLS = {
-    BACKEND_URL: "https://spa-booking-backend-kcqy.onrender.com", // Backend API base URL
+    BACKEND_URL: "http://34.171.50.142:3000", // Backend API base URL
   };
   
   export const DEFAULT_VALUES = {
