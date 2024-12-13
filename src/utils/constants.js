6093064c9d3f4f9c341e5a0a3ab455577ae8560b
Muhammed Irshad Ali K K <75845752+M0I0A@users.noreply.github.com>
2024-12-13 17:00:27 +0530

    url changed

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,7 +1,7 @@
 export const API_URLS = {
-    BACKEND_URL: "http://34.171.50.142:3000", // Backend API base URL
-  };
-  
-  export const DEFAULT_VALUES = {
-    IMAGE_URL: "https://www.dermaessentia.com/cdn/shop/articles/Hair-Spa-for-Men.jpg?v=1694420768",
-  };
+  BACKEND_URL: "https://spa-booking-backend-kcqy.onrender.com", // Backend API base URL
+};
+
+export const DEFAULT_VALUES = {
+  IMAGE_URL: "https://www.dermaessentia.com/cdn/shop/articles/Hair-Spa-for-Men.jpg?v=1694420768",
+};
